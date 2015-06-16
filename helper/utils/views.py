0df7fdb61df12f70328b9e3f27e5117ee6cac352
Daniel Schep <dschep@gmail.com>
2015-06-16 13:00:24 -0400

    durrr

--- helper/utils/views.py
+++ helper/utils/views.py
@@ -8,7 +8,7 @@ class OAuth2Login(View):
     authorization_url = None
     token_url = None
     extra_save = None
-    scopes = None
+    scopes = []
 
     def get(self, request, agent_config):
         if 'code' in request.GET:
@@ -19,8 +19,6 @@ class OAuth2Login(View):
                 'redirect_uri': request.build_absolute_uri().split('?')[0],
                 'grant_type': 'authorization_code',
             }
-            if self.scopes is not None:
-                params['scope'] = ','.join(self.scopes)
             resp = requests.post(self.token_url, data=params)
             resp.raise_for_status()
 
@@ -35,6 +33,8 @@ class OAuth2Login(View):
         else:
             return redirect(self.authorization_url +
                             ('?client_id={app_id}&response_type=code'
-                             '&redirect_uri={redirect_uri}').format(
+                             '&redirect_uri={redirect_uri}&scope={scope}').format(
                                  app_id=agent_config.options['app_id'],
-                                 redirect_uri=request.build_absolute_uri()))
+                                 redirect_uri=request.build_absolute_uri(),
+                                 scope=','.join(self.scopes),
+                             ))
