7b0e3d3b9e747545f1c154ec0ce0b00828801ea2
Daniel Schep <dschep@gmail.com>
2015-06-16 12:26:37 -0400

    github requires user config option!

--- helper/agents/github/__init__.py
+++ helper/agents/github/__init__.py
@@ -1,6 +1,6 @@
 from . import tasks
 
-USER_CONFIG_KEYS = ['access_token']
+USER_CONFIG_KEYS = ['user', 'access_token']
 ACTION_CONFIG_KEYS = {}
 CONFIG_KEYS = USER_CONFIG_KEYS + list(ACTION_CONFIG_KEYS.keys())
 CONFIG_ACTIONS = []
