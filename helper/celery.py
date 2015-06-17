1d16095771a6969bd5f8621747c3eae3c1016c9d
Daniel Schep <dschep@gmail.com>
2015-06-16 21:13:23 -0400

    switch to redis. holy shit that was easy

--- helper/celery.py
+++ helper/celery.py
@@ -4,7 +4,7 @@ from celery import Celery
 
 from django.conf import settings
 
-app = Celery('helper', broker='amqp://', backend='amqp://')
+app = Celery('helper', broker='redis://', backend='redis://')
 
 # Using a string here means the worker will not have to
 # pickle the object when using Windows.
