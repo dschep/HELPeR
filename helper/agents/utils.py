035f0e8e32dbdedca1fc2044469650f3212631ea
Daniel Schep <dschep@gmail.com>
2015-06-23 20:12:29 -0400

    de-json when loading from event store, if you want raw dont use helper funcs. duh

--- helper/agents/utils.py
+++ helper/agents/utils.py
@@ -4,8 +4,10 @@ from ..models import Event
 
 
 def get_from_event_store(task_pair_id, task_type):
-    return Event.objects.filter(task_pair__id=task_pair_id, task_type=task_type)\
-        .values_list('data', flat=True)
+    return [{k: json.loads(v) for k, v in event.items()}
+            for event in Event.objects.filter(
+                task_pair__id=task_pair_id, task_type=task_type)\
+            .values_list('data', flat=True)]
 
 def send_to_event_store(data, task_pair_id, task_type):
     Event.objects.create(data={k: json.dumps(v) for k, v in data.items()},
