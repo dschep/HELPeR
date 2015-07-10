22254cc79e7d507f08330dbd80ddc56cab61a2ab
Daniel Schep <dschep@gmail.com>
2015-07-10 12:35:45 -0400

    whoops, local settings in env too

--- helper/settings.py
+++ helper/settings.py
@@ -1,6 +1,19 @@
 import os
 SECRET_KEY=os.environ.get('SECRET_KEY')
 
+EMAIL_HOST = os.environ.get('EMAIL_HOST')
+EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
+EMAIL_PORT = int(os.environ.get('EMAIL_PORT', '25'))
+EMAIL_USE_TLS = os.environ.get('EMAIL_USE_TLS', 'false').lower() == 'true'
+DEFAULT_FROM_EMAIL = os.environ.get('DEFAULT_FROM_EMAIL')
+SERVER_EMAIL = os.environ.get('SERVER_EMAIL')
+
+ADMINS = (('', os.environ.get('ADMIN_EMAIL')), )
+
+CELERY_SEND_TASK_ERROR_EMAILS = os.environ.get('EMAIL_USE_TLS', 'false').lower() == 'true'
+
+
 DEBUG = False
 
 ALLOWED_HOSTS = ['*']
